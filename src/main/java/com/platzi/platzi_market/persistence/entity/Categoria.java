28560aba3db9d01b32a9626354f944273db3760b
ncastro@vozy.co <ncastro@vozy.co>
2025-01-30 00:34:52 -0300

    add Categoria

---
+++ src/main/java/com/platzi/platzi_market/persistence/entity/Categoria.java
@@ -0,0 +1,13 @@
+package com.platzi.platzi_market.persistence.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categorias")
+
+public class Categoria {
+
+    
+
+}
