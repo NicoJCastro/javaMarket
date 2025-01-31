7d788bd1813a9a60870e1bf03a95acdd54153f07
Nicolas Castro <nicolasjosecastro@gmail.com>
2025-01-30 23:12:07 -0300

    add queryethods

---
+++ src/main/java/com/platzi/platzi_market/persistence/ProductoRepository.java
@@ -0,0 +1,23 @@
+package com.platzi.platzi_market.persistence;
+
+import com.platzi.platzi_market.persistence.crud.ProductoCrudRepository;
+import java.util.List;
+import java.util.Optional;
+
+import com.platzi.platzi_market.persistence.entity.Producto;
+
+public class ProductoRepository {
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll() {
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategory(int idCategory) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategory);
+    }
+
+    public Optional<List<Producto>> getScarceProducts(int quantity) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+    }
+}
