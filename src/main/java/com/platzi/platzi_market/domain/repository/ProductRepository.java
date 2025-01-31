47e7cc8852494c87674e10c4b8e86f2b8ba4f389
Nicolas Castro <nicolasjosecastro@gmail.com>
2025-01-31 11:52:44 -0300

    add data mapper

---
+++ src/main/java/com/platzi/platzi_market/domain/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.platzi.platzi_market.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.platzi.platzi_market.domain.Product;
+
+public interface ProductRepository {
+    List<Product> getAll();
+    Optional<List<Product>> getByCategory(int categoryId);
+    List<Product> getScarceProducts(int quantity);
+    Optional<Product> getProduct(int productId);
+    Product save(Product product);
+    void delete(int productId);
+}
--- src/main/java/com/platzi/platzi_market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/platzi_market/persistence/ProductoRepository.java
@@ -4,8 +4,11 @@ import com.platzi.platzi_market.persistence.crud.ProductoCrudRepository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Repository;
+
 import com.platzi.platzi_market.persistence.entity.Producto;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -21,5 +24,19 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getScarceProducts(int quantity) {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+    }
+
+    // Interactuar con la BD metodos de guardar, eliminar, etc
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
     }
 }
